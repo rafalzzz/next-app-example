edb63e93b373df919de6d0627c951d2bf34748e7
unknown <rzdeb94@gmaile.com>
2022-10-18 20:56:23 +0200

    add urls to articles

--- shared/components/form-fields/number-field-with-mask.tsx
+++ shared/components/form-fields/number-field-with-mask.tsx
@@ -1,22 +1,36 @@
+import { PatternFormat } from "react-number-format";
 import { ExtendedFormField } from "types/extended-form-field";
+
 import * as Styled from "./index.styled";
 
 type TextFieldProps = {
   formField: ExtendedFormField;
 };
 
+//Articles how to solve problem with NumerFieldWithMask console error
+//https://codesandbox.io/s/late-feather-shr01?file=/src/App.js:391-401
+//https://react-hook-form.com/get-started#IntegratingControlledInputs
 export const NumberFieldWithMask = ({ formField }: TextFieldProps) => {
-  const { key, label, register, error, format, allowEmptyFormatting, mask, onClick } = formField;
+  const {
+    key,
+    label,
+    register,
+    error,
+    format,
+    allowEmptyFormatting,
+    mask,
+    onClick,
+  } = formField;
 
   return (
     <Styled.Label key={key}>
       <Styled.InputName>{`${label}:`}</Styled.InputName>
       <Styled.InputContainer>
-        <Styled.NumberInputWithMask
+        <PatternFormat
+          customInput={Styled.NumberInputWithMask}
           format={format ?? ""}
           allowEmptyFormatting={allowEmptyFormatting}
           mask={mask}
-          width={250}
           {...register}
         />
         <Styled.Button type="button" onClick={onClick}>
