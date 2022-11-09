a660ba99490f61b5874a3f464db956a254e5c44e
unknown <rzdeb94@gmaile.com>
2022-11-09 02:04:27 +0100

    fix renderWithProviders Typescript error

--- shared/test-utils/render-with-props.tsx
+++ shared/test-utils/render-with-props.tsx
@@ -1,24 +1,31 @@
-import React, { JSXElementConstructor, ReactElement } from "react";
+import React, { PropsWithChildren } from "react";
 import { Provider } from "react-redux";
 import { ToastContainer } from "react-toastify";
+import { PreloadedState } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
-import { render } from "@testing-library/react";
-import { setupStore } from "store";
+import { render, RenderOptions } from "@testing-library/react";
+import { AppStore, RootState, setupStore } from "store";
 import { ThemeProvider } from "styled-components";
 import { light } from "styles/themes/light";
 
+interface ExtendedRenderOptions extends Omit<RenderOptions, "queries"> {
+  preloadedState?: PreloadedState<RootState>;
+  store?: AppStore;
+}
+
 export function renderWithProviders(
-  ui: ReactElement<unknown, string | JSXElementConstructor<unknown>>,
-  //@ts-ignore
+  ui: React.ReactElement,
   {
     preloadedState = {},
+    // Automatically create a store instance if no store was passed in
     store = setupStore(preloadedState),
     ...renderOptions
-  } = {}
+  }: ExtendedRenderOptions = {}
 ) {
   setupListeners(store.dispatch);
 
-  function Wrapper({ children }: { children: JSX.Element }) {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  function Wrapper({ children }: PropsWithChildren<{}>): JSX.Element {
     return (
       <Provider store={store}>
         <ThemeProvider theme={light}>
